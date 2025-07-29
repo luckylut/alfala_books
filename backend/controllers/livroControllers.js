aa5e29d6a636a0b31d96a981b98345eba3f027f2
VITOR SILVA GOMES <vitor.sgomes2@senacsp.edu.br>
2025-07-28 22:02:01 -0300

    comentario

--- backend/controllers/livroControllers.js
+++ backend/controllers/livroControllers.js
@@ -1,10 +1,10 @@
 
-import { getTodosLivros, getLivroPorld, insereLivro, modificaLivro, deletaLivro } from "../services/livrosServices.js";
+import { getTodosLivros, getLivroPorId, insereLivro, modificaLivro, deletaLivroPorId } from '../Services/livroServices.js';
 
 export const getLivros = (req, res) => {
     try {
         const livros = getTodosLivros();
-        res.send('livros');
+        res.send(livros);
     } catch (error) {
         res.status(500);
         res.send(error.message);
@@ -14,40 +14,63 @@ export const getLivros = (req, res) => {
 export const getLivro = (req, res) => {
     try {
         const id = req.params.id
-        const livro = getLivroPorld(id)
-        res.send(livro)
+
+        if (id && Number(id)) {
+            const livro = getLivroPorId(id)
+            res.send(livro)
+
+        } else {
+            res.status(422)
+            res.send("Id Inválido")
+        }
     } catch (error) {
         res.status(500)
         res.send(error.message)
     }
 }
 
-export const postLivro = async (req, res) =>{
-    try{
-        const LivroNovo = req.body;
-        await insereLivro(LivroNovo);
-        res.status(201).json(LivroNovo)
-    }catch{
-        res.status.status(500).send(error.message);
-    }
-}
-
-export const patchLivro = async (req, res) => {
+export const postLivro = (req, res) => {
     try {
-        const id = req.param.id
-        const body = req.body
-        modificaLivro(body, id)
-        res.send("Livro modificado com sucesso")
+        const livroNovo = req.body;
+        if (req.body.nome) {
+            insereLivro(livroNovo);
+            res.status(201).json(livroNovo);
+        } else {
+            res.status(422)
+            res.send("O campo nome é obrigatório")
+        }
     } catch (error) {
         res.status(500).send(error.message);
     }
 }
 
+export const patchLivro = (req, res) => {
+    try {
+        const id = req.params.id
+        const body = req.body
+        if (id && Number(id)) {
+            modificaLivro(body, id)
+            res.send("Livro modificado com sucesso");
+        } else {
+            res.status(422)
+            res.send("Id inválido")
+        }
+
+    } catch (error) {
+        res.status(500)
+        res.send(error.message)
+    }
+};
 export const deletaLivro = (req, res) => {
     try {
         const id = req.params.id
-        deletaLivroPorId(id)
-        res.send("livro deletado com sucesso")
+        if (id && Number(id)) {
+            deletaLivroPorId(id)
+            res.send("livro deletado com sucesso")
+        } else {
+            res.status(422)
+            res.send("Id inválido")
+        }
     } catch (error) {
         res.status(500)
         res.send(error.message)
